4c2771f31062aefe86558b9c4c83e6dc4dd06c95
Callum <callum.waddell@btinternet.com>
2015-08-24 16:30:48 +0100

    Basic Movement
    Added walk and turn
    Added camera shake
    Added some buildings

---
+++ Blockzila/Assets/Custom Assets/BlockzialaEvents.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockzialaEvents : MonoBehaviour {
+
+    public Animator animatorCam;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ShakeCamera()
+    {
+        animatorCam.SetTrigger("Shake");
+    }
+
+}
---
+++ Blockzila/Assets/Custom Assets/Blockzila.cs
@@ -0,0 +1,46 @@
+﻿    using UnityEngine;
+using System.Collections;
+
+public class Blockzila : MonoBehaviour {
+
+
+    public float speed = 1;
+    public float turnSpeed = 1;
+
+    CharacterController characterController;
+
+    public Animator animatorCam;
+    public Animator animatorModel;
+
+    // Use this for initialization
+    void Start ()
+    {
+        characterController = GetComponent<CharacterController>();
+
+         
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+
+        Vector3 move = transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
+        characterController.Move(move);
+
+        Vector3 turn = Vector3.up * Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+
+        characterController.transform.Rotate(turn);
+
+
+        animatorModel.SetFloat("Speed", characterController.velocity.magnitude);
+
+    }
+
+
+
+}
