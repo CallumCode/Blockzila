c6993eb72e40e74631b8c4e9e50b58e71b8e58ce
callumw <callum.waddell@btinternet.com>
2015-09-21 22:14:41 +0100

    Start the police

---
+++ Blockzila/Assets/Custom Assets/PoliceCar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoliceCar : MonoBehaviour {
+
+
+    NavMeshAgent navMeshAgent;
+    public Transform target = null;
+    // Use this for initialization
+    void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        if(target)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+
+    }
+}
